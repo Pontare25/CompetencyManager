7cf6479b675dc7d478147692de27534890954269
PontusN <PontusN@172.20.10.3>
2018-12-22 18:24:19 +0100

    Added Controller class and connected the textfields to their respective FXML components.

---
+++ src/employee/view/AddNewEmployeeController.java
@@ -0,0 +1,18 @@
+package employee.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AddNewEmployeeController {
+    
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField phoneField;
+    @FXML
+    private TextField emailField;
+    
+    
+    
+    
+}
